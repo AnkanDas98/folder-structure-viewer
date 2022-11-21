f47523de9551d0e29cb5978e96c7b48675812f2f
Ankan Das <ankan.das96@gmail.com>
2022-11-21 14:00:59 +0600

    fix url

--- client/src/request.js
+++ client/src/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:8000/api";
-const BASE_URL = "/api";
+const BASE_URL = "https://simplefolderstructure.herokuapp.com/api";
 
 export const axiosRequest = axios.create({
   baseURL: BASE_URL,
